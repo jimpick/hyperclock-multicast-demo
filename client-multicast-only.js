969dbb511a95c8f8e6749788eb2e660a379ce824
Jim Pick <jim@backblend.com>
2018-07-19 09:28:47 -0700

    Add client-multicast-only
    Doesn't seem to work ... it probably needs some bootstrapping somehow.

---
+++ client-multicast-only.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const crypto = require('hypercore-crypto')
+const swarmDefaults = require('dat-swarm-defaults')
+const discoverySwarm = require('discovery-swarm')
+const mswarm = require('hypercore-multicast-swarm')
+const hyperclock = require('hyperclock')
+const ram = require('random-access-memory')
+
+console.log('Loading clock feed. (Multicast only) !!!! Doesn't work !!!!')
+
+let mode = 'Multicast only'
+
+const key = fs.readFileSync('./key')
+const discoveryKey = crypto.discoveryKey(key) 
+
+const clock = hyperclock(ram, key, {
+  sparse: true,
+  allowPush: true
+})
+
+// UDP Multicast
+const msw = mswarm(clock, {
+  mtu: 900,
+  port: 5007,
+  address: '224.1.1.1'
+})
+
+// Display clock
+clock.ready(() => {
+  console.log('Key:', clock.key.toString('hex'))
+  console.log('Discovery Key:', clock.discoveryKey.toString('hex'))
+  clock.update(() => {
+    console.log('Length', clock.length)
+    clock.createReadStream({live: true, tail: true}).on('data', data => {
+      console.log(`${mode}: ${data.time}`)
+    })
+  })
+})
+
